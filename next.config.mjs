7921f56384dad0408719d44d1e3d60daadf61482
Patil-Shrikrishna <krishnapt1993@gmail.com>
2024-03-12 15:09:58 +0530

    [build]: Instructors component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // protocol: "https",
+    domains: ["images.unsplash.com"],
+    // port: "",
+    // pathname: "/my-bucket/**",
+  },
+};
 
 export default nextConfig;
